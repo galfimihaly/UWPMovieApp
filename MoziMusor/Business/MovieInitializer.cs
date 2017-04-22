c452e88d55963cf487b7497b072e805d2ab473cc
kisscsaba91 <kisscsaba91@gmail.com>
2017-04-22 09:56:37 +0200

    Átalakítjuk json kérés előtt az ékezetes karaktereket, mert néha elírják a malommozinál a címekben az ékezeteket.... :D

--- MoziMusor/Business/MovieInitializer.cs
+++ MoziMusor/Business/MovieInitializer.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace MoziMusor.Business
@@ -32,6 +33,10 @@ namespace MoziMusor.Business
                 jsonModel = new MovieModel();
 
                 preparedTitle = model.title.Replace(" ", "+").Replace("3D", "");
+
+                //kiszedjük az ékezeteket, mert a malommozi rss-ében néha el van írva h hosszúak vagy rövidek, ami miatt
+                //nem találta meg a moviedb a keresett filmet...
+                preparedTitle = Regex.Replace(preparedTitle.Normalize(NormalizationForm.FormD), @"\p{Mn}", "");
 
                 uri = apiManager.GetMovieByTitle(preparedTitle);
 
